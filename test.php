e253b80aaae67f6796e5b81b7e7b75162c61cc35
Andrew <Andrew.the.techie@gmail.com>
2014-04-18 17:53:49 +0000

    added robots.txt

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+
+	echo checkdnsrr("aherrington.com","NS");
+
+	echo "<hr />";
+
+	echo checkdnsrr("iammakinghtisupirghtamdbiaaea.net","NS");
+
+	echo "<hr />";
+
+	echo checkdnsrr("uyunmabdulaklaudaguiahdknkjawhicefg","NS");
+?>
